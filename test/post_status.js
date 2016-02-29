a4bbea50fe4a6e1c43add40cb864f7f2d6798b33
Praylin Diana Dinamoni <36@p1-2.hbtn.io>
2016-02-29 13:04:58 -0800

    Display/block of form

---
+++ test/post_status.js
@@ -0,0 +1,10 @@
+//window.addEventListener('load', displayForm)
+document.addEventListener('DOMContentLoaded', displayForm);
+function displayForm()
+	{
+		var myForm = document.getElementById('post_status');
+		myForm.style.display = "block";
+	}
+//document.addEventListener('DOMContentLoaded', function(){
+
+//1})

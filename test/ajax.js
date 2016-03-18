b7dc782c90260ea26eed557e28d1057d73c63812
praylin <rampoche@macbooks-MacBook-Pro.local>
2016-03-17 17:46:31 -0700

    Task 0 working in test file

---
+++ test/ajax.js
@@ -0,0 +1,7 @@
+var onSuccess = function(extra_statuses)
+{
+}
+var ajaxGet = function(url, onSuccess) {
+	var xhttp = new XMLHttpRequest();
+
+}
--- test/load_more.js
+++ test/load_more.js
@@ -2,7 +2,7 @@ function loadStatus() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("morestatus").innerHTML = xhttp.response;
+      document.getElementById("extra_statuses").innerHTML = xhttp.responseText;
     }
   };
   xhttp.open("GET", "statuses-1.html", true);

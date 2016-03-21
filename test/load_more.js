2836ad4019280ea4af8ef5e00cd921fe941db425
praylin <rampoche@macbooks-MBP.hsd1.ca.comcast.net>
2016-03-20 19:52:42 -0700

    Mandatory tasks done in test folder

--- test/load_more.js
+++ test/load_more.js
@@ -1,10 +1,3 @@
-function loadStatus() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("extra_statuses").innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", "statuses-1.html", true);
-  xhttp.send();
+function onSuccess(xhttp) {
+    document.getElementById("extra_statuses").innerHTML = xhttp.responseText;
 }

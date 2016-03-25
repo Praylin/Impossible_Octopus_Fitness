2f3db2085e1db03e38737b0d21c6b18022a48669
praylin <rampoche@macbooks-MacBook-Pro.local>
2016-03-25 13:06:03 -0700

    Menu elements hovering on mouse pointing

--- post_a_status.js
+++ post_a_status.js
@@ -2,8 +2,10 @@
 document.addEventListener('DOMContentLoaded', displayForm);
 function displayForm(){
 	var theButton = document.getElementById('post_status_button');
-		theButton.addEventListener("click", function() {
-			var poststatus = document.getElementById('post_status');
-			poststatus.toggle();
-			})
+	theButton.addEventListener("click", function() {
+		var poststatus = document.getElementById('post_status');
+		console.log(poststatus);
+		poststatus.toggle();
+		//poststatus.addEventListener("click",toggle,true);
+		})
 }
--- reply.js
+++ reply.js
@@ -1,8 +1,6 @@
 //Reply button for each status
 document.addEventListener('DOMContentLoaded', replyFunction);
-
 function replyFunction(){
-
 	var elements = document.querySelectorAll('.reply_button');
 	[].map.call(elements, function(elem) {
 		elem.addEventListener('click', toggle, true)

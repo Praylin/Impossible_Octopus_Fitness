a37fbc624f03deccd5d191e81a341b98395d1f4f
praylin <rampoche@macbooks-MacBook-Pro.local>
2016-03-25 18:29:27 -0700

    Post status button working

--- post_a_status.js
+++ post_a_status.js
@@ -1,11 +1,9 @@
 //Post a status
+
 document.addEventListener('DOMContentLoaded', displayForm);
 function displayForm(){
-	var theButton = document.getElementById('post_status_button');
-	theButton.addEventListener("click", function() {
-		var poststatus = document.getElementById('post_status');
-		console.log(poststatus);
-		poststatus.toggle();
-		//poststatus.addEventListener("click",toggle,true);
-		})
+	var elements = document.querySelectorAll('.post_status_button');
+	[].map.call(elements, function(elem) {
+		elem.addEventListener('click', toggle, true)
+	})
 }
--- toggle.js
+++ toggle.js
@@ -1,8 +1,7 @@
 //Toggle Function
 function toggle(index) {
-	console.log('toggle', this.dataset.index)
 	var element = document.getElementById(this.dataset.index);
-	if (element.style.display === 'block')
+	if (element.style.display == 'block')
 		element.style.display = 'none';
 	else
 		element.style.display = "block";
